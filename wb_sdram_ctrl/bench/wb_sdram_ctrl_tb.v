65c6fae2d8b360e8eb1839dbc574f9fdcfcb2269
LI Tao <tli@techyauld.com>
2019-02-07 09:38:17 +0800

    initial commit

--- wb_sdram_ctrl/bench/wb_sdram_ctrl_tb.v
+++ wb_sdram_ctrl/bench/wb_sdram_ctrl_tb.v
@@ -23,6 +23,8 @@
  * WORK, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+`timescale 1 ns / 1 ns
+
 module wb_sdram_ctrl_tb;
 
    parameter technology = "GENERIC";
@@ -59,78 +61,78 @@ module wb_sdram_ctrl_tb;
    assign #1 sdram_clk_d = sdram_clk;
 
    wire [BA_WIDTH-1:0] ba;
-   wire [12:0] 	       a;
-   wire 	       cs_n;
-   wire 	       ras;
-   wire 	       cas;
-   wire 	       we;
-   wire [15:0] 	       dq_o;
-   wire [1:0] 	       dqm;
-   wire [15:0] 	       dq_i;
-   wire 	       dq_oe;
-   wire 	       cke;
+   wire [12:0]         a;
+   wire            cs_n;
+   wire            ras;
+   wire            cas;
+   wire            we;
+   wire [15:0]         dq_o;
+   wire [1:0]          dqm;
+   wire [15:0]         dq_i;
+   wire            dq_oe;
+   wire            cke;
 
-   wire [15:0] 	       dq;
+   wire [15:0]         dq;
 
    wire [WB_PORTS*32-1:0] wb_adr;
-   wire [WB_PORTS-1:0] 	  wb_stb;
-   wire [WB_PORTS-1:0] 	  wb_cyc;
+   wire [WB_PORTS-1:0]    wb_stb;
+   wire [WB_PORTS-1:0]    wb_cyc;
    wire [WB_PORTS*3-1:0]  wb_cti;
    wire [WB_PORTS*2-1:0]  wb_bte;
-   wire [WB_PORTS-1:0] 	  wb_we;
+   wire [WB_PORTS-1:0]    wb_we;
    wire [WB_PORTS*4-1:0]  wb_sel;
    wire [WB_PORTS*32-1:0] wb_dat;
    wire [WB_PORTS*32-1:0] wb_rdt;
-   wire [WB_PORTS-1:0] 	  wb_ack;
+   wire [WB_PORTS-1:0]    wb_ack;
 
-   wire [31:0] 	 slave_writes;
-   wire [31:0] 	 slave_reads;
+   wire [31:0]   slave_writes;
+   wire [31:0]   slave_reads;
    wire [WB_PORTS-1:0] done_int;
 
-   genvar 	 i;
+   genvar    i;
 
-   integer 	 TRANSACTIONS;
-   integer 	 SUBTRANSACTIONS;
-   integer 	 SEED;
+   integer   TRANSACTIONS;
+   integer   SUBTRANSACTIONS;
+   integer   SEED;
 
    generate
       for(i=0;i<WB_PORTS;i=i+1) begin : masters
-	 wb_bfm_transactor
-	    #(.MEM_HIGH (MEMORY_SIZE_WORDS*(i+1)-1),
-	      .MEM_LOW  (MEMORY_SIZE_WORDS*i),
-	      .AUTORUN  (0),
-	      .VERBOSE  (0))
-	 bfm
-	    (.wb_clk_i (wb_clk),
-	     .wb_rst_i (wbm_rst),
-	     .wb_adr_o (wb_adr[i*32+:32]),
-	     .wb_dat_o (wb_dat[i*32+:32]),
-	     .wb_sel_o (wb_sel[i*4+:4]),
-	     .wb_we_o  (wb_we[i] ),
-	     .wb_cyc_o (wb_cyc[i]),
-	     .wb_stb_o (wb_stb[i]),
-	     .wb_cti_o (wb_cti[i*3+:3]),
-	     .wb_bte_o (wb_bte[i*2+:2]),
-	     .wb_dat_i (wb_rdt[i*32+:32]),
-	     .wb_ack_i (wb_ack[i]),
-	     .wb_err_i (1'b0),
-	     .wb_rty_i (1'b0),
-	     //Test Control
-	     .done(done_int[i]));
+     wb_bfm_transactor
+        #(.MEM_HIGH (MEMORY_SIZE_WORDS*(i+1)-1),
+          .MEM_LOW  (MEMORY_SIZE_WORDS*i),
+          .AUTORUN  (0),
+          .VERBOSE  (0))
+     bfm
+        (.wb_clk_i (wb_clk),
+         .wb_rst_i (wbm_rst),
+         .wb_adr_o (wb_adr[i*32+:32]),
+         .wb_dat_o (wb_dat[i*32+:32]),
+         .wb_sel_o (wb_sel[i*4+:4]),
+         .wb_we_o  (wb_we[i] ),
+         .wb_cyc_o (wb_cyc[i]),
+         .wb_stb_o (wb_stb[i]),
+         .wb_cti_o (wb_cti[i*3+:3]),
+         .wb_bte_o (wb_bte[i*2+:2]),
+         .wb_dat_i (wb_rdt[i*32+:32]),
+         .wb_ack_i (wb_ack[i]),
+         .wb_err_i (1'b0),
+         .wb_rty_i (1'b0),
+         //Test Control
+         .done(done_int[i]));
 
-	 initial begin
-	    //Grab CLI parameters
-	    if($value$plusargs("transactions=%d", TRANSACTIONS))
-	      bfm.set_transactions(TRANSACTIONS);
-	    if($value$plusargs("subtransactions=%d", SUBTRANSACTIONS))
-	      bfm.set_subtransactions(SUBTRANSACTIONS);
-	    if($value$plusargs("seed=%d", SEED))
-	      bfm.SEED = SEED;
+     initial begin
+        //Grab CLI parameters
+        if($value$plusargs("transactions=%d", TRANSACTIONS))
+          bfm.set_transactions(TRANSACTIONS);
+        if($value$plusargs("subtransactions=%d", SUBTRANSACTIONS))
+          bfm.set_subtransactions(SUBTRANSACTIONS);
+        if($value$plusargs("seed=%d", SEED))
+          bfm.SEED = SEED;
 
-	    bfm.display_settings;
-	    bfm.run;
-	    bfm.display_stats;
-	 end
+        bfm.display_settings;
+        bfm.run;
+        bfm.display_stats;
+     end
       end // block: slaves
    endgenerate
 
@@ -138,27 +140,27 @@ module wb_sdram_ctrl_tb;
 
    always @(done) begin
       if(done === 1) begin
-	 $display("All tests passed!");
-	 $finish;
+     $display("All tests passed!");
+     $finish;
 
       end
    end
 
 wb_sdram_ctrl
-  #(.TECHNOLOGY	(technology),
-    .CLK_FREQ_MHZ	(100),	// sdram_clk freq in MHZ
-    .POWERUP_DELAY	(0),	// power up delay in us
-    .BURST_LENGTH	(8),	// 0), 1), 2), 4 or 8 (0 = full page)
-    .WB_PORTS	(WB_PORTS),	// Number of wishbone ports
-    .BUF_WIDTH	(3),	// Buffer size = 2^BUF_WIDTH
-    .ROW_WIDTH	(13),	// Row width
-    .COL_WIDTH	(9),	// Column width
-    .BA_WIDTH	(BA_WIDTH),	// Ba width
-    .tCAC		(2),	// CAS Latency
-    .tRAC		(5),	// RAS Latency
-    .tRP		(2),	// Command Period (PRE to ACT)
-    .tRC		(7),	// Command Period (REF to REF / ACT to ACT)
-    .tMRD		(3))	// Mode Register Set To Command Delay time
+  #(.TECHNOLOGY (technology),
+    .CLK_FREQ_MHZ   (100),  // sdram_clk freq in MHZ
+    .POWERUP_DELAY  (0),    // power up delay in us
+    .BURST_LENGTH   (8),    // 0), 1), 2), 4 or 8 (0 = full page)
+    .WB_PORTS   (WB_PORTS), // Number of wishbone ports
+    .BUF_WIDTH  (3),    // Buffer size = 2^BUF_WIDTH
+    .ROW_WIDTH  (13),   // Row width
+    .COL_WIDTH  (9),    // Column width
+    .BA_WIDTH   (BA_WIDTH), // Ba width
+    .tCAC       (2),    // CAS Latency
+    .tRAC       (5),    // RAS Latency
+    .tRP        (2),    // Command Period (PRE to ACT)
+    .tRC        (7),    // Command Period (REF to REF / ACT to ACT)
+    .tMRD       (3))    // Mode Register Set To Command Delay time
    wb_sdram_ctrl0
    (// SDRAM interface
     .sdram_rst  (sdram_rst),
@@ -174,7 +176,7 @@ wb_sdram_ctrl
     .dq_i       (dq_i),
     .dq_oe      (dq_oe),
     .cke_pad_o  (cke),
-		 // Wishbone interface
+         // Wishbone interface
     .wb_clk   (wb_clk),
     .wb_rst   (wb_rst),
     .wb_adr_i (wb_adr),
@@ -204,5 +206,18 @@ wb_sdram_ctrl
       .Cas_n (cas),
       .We_n  (we),
       .Dqm   (dqm));
+
+  //  // Dump waves
+  // initial begin
+  //   $dumpfile("dump.vcd");
+  //   $dumpvars(0, wb_sdram_ctrl_tb);
+  // end
+
+`ifdef DUMP_FSDB
+   initial begin
+      $fsdbDumpfile("test.fsdb");
+      $fsdbDumpvars;
+   end
+`endif
 
 endmodule
